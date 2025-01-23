7dc547b6bcebe21db15c1263124b23907d975fb3
Michal Kalinowski <kalinowskimichal638@gmail.com>
2025-01-23 23:04:48 +0100

    css

--- app/zadanie6a/index.php
+++ app/zadanie6a/index.php
@@ -13,6 +13,7 @@ if (! isset($_SESSION["zadanie6a-logged-in"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kalinowski</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="/assets/global.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
--- app/zadanie6b/auth/login-form.php
+++ app/zadanie6b/auth/login-form.php
@@ -9,6 +9,7 @@ if (isset($_SESSION["zadanie6b-logged-in"])) {
 <head>
     <title>Kalinowski</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="/assets/global.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
